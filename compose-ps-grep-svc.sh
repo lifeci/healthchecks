5c962d29e5f786a8d6618babce2e7f6f7fa00f42
Roman Judin <31796644+rjudin@users.noreply.github.com>
2018-12-03 16:11:42 +0100

    Fixed edge case logic

--- compose-ps-grep-svc.sh
+++ compose-ps-grep-svc.sh
@@ -4,9 +4,9 @@ Delay=$2;
 if [ -z $SvcName ]; then echo 'USAGE: compose-ps-grep-svc.sh $SvcName $Delay' && exit -2; fi
 if [ -z $Delay ]; then Delay=1; fi
 
-#unset COUNTER; unset health; unset healthy;
+unset COUNTER; unset health; unset healthy;
 
-while [[ "$COUNTER" -lt 10 ]] || [[ "$healthy" != 'true' ]]; do
+while  [[ "$COUNTER" -lt 10 ]] && [[ "$healthy" != 'true' ]]; do
   health=$(docker-compose  ps | grep $SvcName);
   let COUNTER=COUNTER+1; sleep $Delay;
   if [[ "$health" == *"Up (healthy)"* ]]; then
@@ -17,7 +17,7 @@ while [[ "$COUNTER" -lt 10 ]] || [[ "$healthy" != 'true' ]]; do
   fi;
 done
 
-if [ "$healthy" == 'true' ]; then
+if [[ "$healthy" == 'true' ]]; then
   echo "ALL RIGHT!";
   exit 0
 else
