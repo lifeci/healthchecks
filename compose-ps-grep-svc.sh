72e443326be7a71e01d479eaa0fb4365e2e8757e
Roman Judin <31796644+rjudin@users.noreply.github.com>
2018-12-03 14:57:57 +0100

    initial commit
    USAGE: compose-ps-grep-svc.sh $SvcName $Delay

---
+++ compose-ps-grep-svc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+SvcName=$1
+Delay=$2;
+if [ -z $SvcName ]; then echo 'USAGE: compose-ps-grep-svc.sh $SvcName $Delay'; fi
+if [ -z $Delay ]; then Delay=1; fi
+
+#unset COUNTER; unset health; unset healthy;
+
+while [[ "$COUNTER" -lt 10 ]]; do
+  health=$(docker-compose  ps | grep $SvcName);
+  let COUNTER=COUNTER+1; sleep $Delay;
+  if [[ "$health" == *"Up (healthy)"* ]]; then
+    printf "$COUNTER | $health\n";
+    healthy=true
+    exit
+  else
+    printf "$COUNTER | $health\n";
+  fi;
+done
+
+if [ "$healthy" == 'true' ]; then
+  echo "ALL RIGHT!";
+  exit 0
+else
+  echo "ALL BAD :(";
+  exit -1
+fi
