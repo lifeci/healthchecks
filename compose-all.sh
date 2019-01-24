d77c8a68bec2e01fe9c9906dac00188a637ba58c
Roman Judin <31796644+rjudin@users.noreply.github.com>
2019-01-24 19:47:15 +0100

    Update compose-all.sh

--- compose-all.sh
+++ compose-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Delay=$1;
-if [ -z $Delay ]; then Delay=3; fi;
+DelayInput=$1;
+if [ -z $Delay ]; then Delay=$DelayInput; fi;
 
 unset HealthKV; declare -A HealthKV;
 # Color output in bash https://goo.gl/DsMWYq
